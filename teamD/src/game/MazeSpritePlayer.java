f4b0e60fe2031e50b9e2b8dbf2c17ea2405b84a5
s1671050 <s1671050@PC132G082.ku-kinds.konan-u.ac.jp>
2017-12-08 16:17:56 +0900

    楠本です。

---
+++ teamD/src/game/MazeSpritePlayer.java
@@ -0,0 +1,17 @@
+package game;
+
+import framework.game2D.Sprite;
+
+/**
+ * –À˜HƒQ[ƒ€‚Å“oê‚·‚éƒvƒŒƒCƒ„[
+ * 
+ * @author T.Kuno
+ * 
+ */
+public class MazeSpritePlayer extends Sprite {
+
+	public MazeSpritePlayer(String imageFile) {
+		super(imageFile);
+	}
+
+}
